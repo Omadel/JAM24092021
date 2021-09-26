8b02017f1b3c38c7ad8318e44197fd21f41bd4ce
EtienneJavelle <83096541+Omadel@users.noreply.github.com>
2021-09-26 18:31:09 +0200

    Blue Range

--- Assets/Scripts/FriendCage.cs
+++ Assets/Scripts/FriendCage.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider))]
 public class FriendCage : Destructible {
+    [SerializeField] private Color rangeColor = Color.blue;
+    private void Start() {
+        MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+        renderers[renderers.Length - 1].material.color = rangeColor;
+    }
     public override int DoExplotion(int strenght) {
         Bomber.Instance.SaveAFriend(transform.position);
         return base.DoExplotion(strenght);
