f56562650de6f5be0c08c9779636a9445eaa38c1
EtienneJavelle <83096541+Omadel@users.noreply.github.com>
2021-09-26 18:22:01 +0200

    Star and animations

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,13 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class Animation : MonoBehaviour {
+    [SerializeField] private float animationHeight, animationDuration;
+
+    private void Start() {
+        transform.DORotate(new Vector3(0, 360, 0), animationDuration, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+        transform.DOMoveY(transform.localPosition.y + animationHeight, animationDuration / 3)
+            .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
+}
